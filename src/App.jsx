fe14eee3f44114c1ba859b105fe4f2f5a307eb28
Sophia Zhang <sophia.p.zhang@gmail.com>
2024-02-17 04:03:22 -0800

    Photoscene creation, buggy authorization
    BUG: The access key generation works but the bearer authorization in createPhotoscene and stuff gives an error 401.

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,104 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const isMounted = useRef(false);
+  if (!isMounted.current) {
+    useEffect(() => {
+      const createAccessToken = async () => {
+        try {
+          const response = await fetch(
+            "https://developer.api.autodesk.com/authentication/v2/token",
+            {
+              method: "POST",
+              headers: {
+                Authorization:
+                  "Basic " +
+                  btoa("8ts8GR4FytJuRuc8VzpWU2e9xJvV6yWr:HEU2kdwTg2D8TxDd"),
+                "Content-Type": "application/x-www-form-urlencoded",
+                Accept: "application/json",
+              },
+              body: new URLSearchParams({
+                grant_type: "client_credentials",
+                scope: "data:write data:read",
+              }),
+            }
+          );
 
-  useEffect(() => {
-    console.log(btoa("8ts8GR4FytJuRuc8VzpWU2e9xJvV6yWr:UwgphvwzOm84Scq3"));
-    const createAccessToken = async () => {
-      try {
-        const response = await fetch(
-          "https://developer.api.autodesk.com/authentication/v2/token",
-          {
-            method: "POST",
-            headers: {
-              Authorization:
-                "Basic " +
-                btoa("8ts8GR4FytJuRuc8VzpWU2e9xJvV6yWr:UwgphvwzOm84Scq3"),
-              "Content-Type": "application/x-www-form-urlencoded",
-              Accept: "application/json",
-            },
-            body: new URLSearchParams({
-              grant_type: "client_credentials",
-              scope: "data:read",
-            }),
+          if (!response.ok) {
+            throw new Error("Failed to create access key");
           }
-        );
 
-        if (!response.ok) {
-          throw new Error("Failed to create photoscene");
+          const jsonData = await response.json();
+          return jsonData.access_token;
+        } catch (error) {
+          console.error("Error creating access key:", error);
         }
+      };
 
-        const jsonData = await response.json();
-        console.log("Photoscene created:", jsonData);
-      } catch (error) {
-        console.error("Error creating photoscene:", error);
-      }
-    };
+      const createPhotoscene = async (token) => {
+        try {
+          console.log("Bearer " + token);
 
-    createAccessToken();
-  }, []);
+          const response = await fetch(
+            "https://developer.api.autodesk.com/photo-to-3d/v1/photoscene",
+            {
+              method: "POST",
+              headers: {
+                Authorization: "Bearer " + token,
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+              body: new URLSearchParams({
+                scenename: "testscene",
+                format: "rcm,rcs,ortho",
+                "metadata_name[0]": "targetcs",
+                "metadata_value[0]": "UTM84-32N",
+              }),
+            }
+          );
+
+          if (!response.ok) {
+            throw new Error("Failed to create photoscene");
+          }
+
+          const jsonData = await response.json();
+          console.log("Photoscene created:", jsonData);
+        } catch (error) {
+          console.error("Error creating photoscene:", error);
+        }
+      };
+
+      const fetchPhotoscene = async (token) => {
+        try {
+          const response = await fetch(
+            "https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/AtAuFsedTdqWdhF9VzHepp5oM9PITiuizI4xdMbz?format=rcm",
+            {
+              method: "GET",
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+
+          if (!response.ok) {
+            throw new Error("Failed to fetch photoscene");
+          }
+
+          const jsonData = await response.json();
+          console.log("Photoscene data:", jsonData);
+        } catch (error) {
+          console.error("Error fetching photoscene:", error);
+        }
+      };
+
+      createAccessToken().then((token) => {
+        createPhotoscene(token);
+        isMounted.current = true;
+      });
+    }, [isMounted]);
+  }
 
   return (
     <>
