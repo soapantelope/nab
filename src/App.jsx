1429141a675e8736b6f8fdaa56cdb0b8f49832a1
Sophia Zhang <sophia.p.zhang@gmail.com>
2024-02-17 16:46:27 -0800

    Scrap Autodesk code, got 3js model displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
 import { useState, useEffect, useRef, Suspense } from "react";
 import "./App.css";
+import Cow from "./cow.png";
 import Nab from "./Nab.jsx";
+import Intro from "./intro.mp4";
 //import Scene from "./components/render3d";
 
 // Following https://aps.autodesk.com/en/docs/reality-capture/v1/tutorials/create-3d-mesh-from-photos/
 function App() {
   return (
-    <>
-      <div style={{ fontSize: "5rem" }}>📸 Nab</div>
-      <main className="container">
-        <h1>Upload a video - get 3D model!</h1>
-        <div className="grid">
-          <div>
-            <form>
-              <label>
-                <input type="file" />
-              </label>
-              <input type="submit" text="submit" />
-            </form>
-            <Nab video="temp"></Nab>
-          </div>
-          <div>
-            {/* {" "}
-            <Scene />{" "} */}
-          </div>
+    <div className="colContainer">
+      {/* IMAGES */}
+      {/* <img className="img" src={Cow}></img> */}
+      {/* NAB */}
+      <Nab video="./intro.mp4"></Nab>
+      <div className="container">
+        <div style={{ fontSize: "2.5rem", fontWeight: "bold" }}>
+          Look at this cute cow I made!
         </div>
-      </main>
-    </>
+        <div style={{ fontSize: "2rem" }}>$15.99</div>
+        <div style={{ fontSize: "1rem" }}>Hand crafted with love.</div>
+      </div>
+    </div>
   );
 }
 
--- src/Nab.jsx
+++ src/Nab.jsx
@@ -1,10 +1,87 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
+import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+
+import { useGLTF } from "@react-three/drei";
+import Glb from "./poly.glb";
+
+export function Model() {
+  const { nodes, materials } = useGLTF(Glb);
+  return (
+    <group dispose={null}>
+      <mesh
+        scale={50}
+        position={[0, -1, 0]}
+        castShadow
+        receiveShadow
+        geometry={nodes.mesh_0.geometry}
+        material={nodes.mesh_0.material}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload(Glb);
 
 // Following https://aps.autodesk.com/en/docs/reality-capture/v1/tutorials/create-3d-mesh-from-photos/
-function Nab(video) {
+function Nab(url) {
   const isMounted = useRef(false);
 
+  async function extractFramesFromVideo(videoUrl, fps = 2) {
+    return new Promise(async (resolve) => {
+      // fully download it first (no buffering):
+      let videoBlob = await fetch(url).then((r) => r.blob());
+      let videoObjectUrl = URL.createObjectURL(videoBlob);
+      let video = document.createElement("video");
+
+      let seekResolve;
+      video.addEventListener("seeked", async function () {
+        if (seekResolve) seekResolve();
+      });
+
+      video.src = videoObjectUrl;
+
+      // workaround chromium metadata bug (https://stackoverflow.com/q/38062864/993683)
+      while (
+        (video.duration === Infinity || isNaN(video.duration)) &&
+        video.readyState < 2
+      ) {
+        await new Promise((r) => setTimeout(r, 1000));
+        video.currentTime = 10000000 * Math.random();
+      }
+      let duration = video.duration;
+
+      let canvas = document.createElement("canvas");
+      let context = canvas.getContext("2d");
+      let [w, h] = [video.videoWidth, video.videoHeight];
+      canvas.width = w;
+      canvas.height = h;
+
+      let frames = [];
+      let interval = 1 / fps;
+      let currentTime = 0;
+
+      while (currentTime < duration) {
+        video.currentTime = currentTime;
+        await new Promise((r) => (seekResolve = r));
+
+        context.drawImage(video, 0, 0, w, h);
+        let base64ImageData = canvas.toDataURL();
+        frames.push(base64ImageData);
+
+        currentTime += interval;
+      }
+      resolve(frames);
+    });
+  }
+
+  URL.revokeObjectURL(url); // revoke URL to prevent memory leak
+
+  // AUTODESK API COULD NOT BE IMPLEMENTED DUE TO AUTHENTICATION ISSUES
   useEffect(() => {
     const createAccessToken = async () => {
       try {
@@ -83,7 +160,7 @@ function Nab(video) {
     const addImages = async (token, photosceneid, photos) => {
       try {
         const response = await fetch(
-          "hhttps://developer.api.autodesk.com/photo-to-3d/v1/file",
+          "https://developer.api.autodesk.com/photo-to-3d/v1/file",
           {
             method: "POST",
             headers: {
@@ -187,9 +264,16 @@ function Nab(video) {
       }
     };
 
+    async function extractFrames(url) {
+      return await extractFramesFromVideo(url, 2);
+    }
+
     createAccessToken().then((token) => {
       createPhotoscene(token).then((photosceneid) => {
-        // parse video into photos
+        // Parse video into photos
+        //const photos = extractFrames(url);
+        // console.log(photos);
+        const photos = [];
         addImages(token, photosceneid, photos).then(() => {
           processPhotoscene(token, photosceneid).then(() => {
             trackPhotoscene(token, photosceneid).then(() => {
@@ -204,7 +288,20 @@ function Nab(video) {
     });
   }, [isMounted]);
 
-  return <div>INSERT NAB HERE</div>;
+  return (
+    <div className="nab">
+      <Canvas>
+        <OrbitControls />
+
+        <Suspense fallback={null}>
+          <Model />
+          <ambientLight intensity={3} />
+          <pointLight position={[10, 10, 10]} />
+          {/* <Environment preset="studio" background /> */}
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default Nab;
