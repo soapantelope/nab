e70de6fe07eaf6e26430af4a5ac92f0f6a88834a
Sophia Zhang <sophia.p.zhang@gmail.com>
2024-02-17 22:38:14 -0800

    WASM branch stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,16 @@ import { useState, useEffect, useRef, Suspense } from "react";
 import "./App.css";
 import Nab from "./Nab.jsx";
 import Intro from "./intro.mp4";
-import FileUploadComponent from "./Uploader.jsx";
-import { FFmpeg } from '@diffusion-studio/ffmpeg-js';
+// import FileUploadComponent from "./Uploader.jsx";
 
 // Following https://aps.autodesk.com/en/docs/reality-capture/v1/tutorials/create-3d-mesh-from-photos/
 function App() {
-
-  const dbURL = "https://pub-8fdcfc93cb6149e48647d45b07b5e003.r2.dev/src/testdata/";
+  const dbURL =
+    "https://pub-8fdcfc93cb6149e48647d45b07b5e003.r2.dev/src/testdata/";
   const [videoUrl, setVideoUrl] = useState(dbURL + "sparkling.glb");
 
-
   const handleFileUpload = (file) => {
-    console.log("Updating File")
-    // Here you would handle the file upload logic
-    // For now, just update the video URL with a new URL
-    setVideoUrl(dbURL + "poly.glb");
-  };
-
-  const handleVideoUpload = (file) => {
-    console.log("Updating File")
+    console.log("Updating File");
     // Here you would handle the file upload logic
     // For now, just update the video URL with a new URL
     setVideoUrl(dbURL + "poly.glb");
@@ -33,11 +24,11 @@ function App() {
         {/* Nab */}
         <Nab videoURL={videoUrl}></Nab>
         {/* File upload component */}
-        <input type="file" onChange={handleFileChange} />
+        {/* <input type="file" onChange={handleFileChange} /> */}
         <button onClick={handleFileUpload}>Upload</button>
         {/* <FileUploadComponent /> */}
       </div>
-      
+
       {/* Right Column */}
       <div className="rightColumn">
         <div className="container">
@@ -78,7 +69,5 @@ function App() {
 //         });
 //     });
 // }
-
-
 
 export default App;
--- src/Nab.jsx
+++ src/Nab.jsx
@@ -12,22 +12,21 @@ import { OrbitControls } from "@react-three/drei";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 // export function Model(url) {
-  // const { nodes, materials } = useGLTF("/poly.glb");
-    // const gltf = useLoader(GLTFLoader, workingURL);
-
+// const { nodes, materials } = useGLTF("/poly.glb");
+// const gltf = useLoader(GLTFLoader, workingURL);
 
 //   return (
 //     // <primitive object={gltf.scene} />
-    // <group dispose={null}>
-    //   <mesh
-    //     scale={50}
-    //     position={[0, -1, 0]}
-    //     castShadow
-    //     receiveShadow
-    //     geometry={nodes.mesh_0.geometry}
-    //     material={nodes.mesh_0.material}
-    //   />
-    // </group>
+// <group dispose={null}>
+//   <mesh
+//     scale={50}
+//     position={[0, -1, 0]}
+//     castShadow
+//     receiveShadow
+//     geometry={nodes.mesh_0.geometry}
+//     material={nodes.mesh_0.material}
+//   />
+// </group>
 //   );
 // }
 
@@ -40,22 +39,22 @@ import { OrbitControls } from "@react-three/drei";
 
 //   return (
 //     <div className="nab">
-      // <Canvas>
-      //   <OrbitControls />
-      //   <Suspense fallback={null}>
-      //     {/* Replace with given URL */}
-      //     <Model url="" />
-      //     <ambientLight intensity={3} />
-      //     <pointLight position={[10, 10, 10]} />
-      //   </Suspense>
-      // </Canvas>
+// <Canvas>
+//   <OrbitControls />
+//   <Suspense fallback={null}>
+//     {/* Replace with given URL */}
+//     <Model url="" />
+//     <ambientLight intensity={3} />
+//     <pointLight position={[10, 10, 10]} />
+//   </Suspense>
+// </Canvas>
 //     </div>
 //   );
 // }
 
 // export default Nab;
-import React, { useState, useEffect, useRef } from 'react';
-import { useGLTF } from '@react-three/drei';
+import React, { useState, useEffect, useRef } from "react";
+import { useGLTF } from "@react-three/drei";
 
 function Nab({ videoURL }) {
   const [url, setURL] = useState(videoURL);
@@ -64,7 +63,7 @@ function Nab({ videoURL }) {
 
   useEffect(() => {
     // console.log(nodes)
-    setURL(videoURL)
+
     // Load glTF resource asynchronously when URL changes
     async function loadGltf(url) {
       try {
@@ -73,31 +72,29 @@ function Nab({ videoURL }) {
         const objectURL = URL.createObjectURL(blob);
         setURL(objectURL);
       } catch (error) {
-        console.error('Error loading glTF:', error);
+        console.error("Error loading glTF:", error);
       }
     }
 
     loadGltf(url);
   }, [url]);
 
-
-function Model() {
-  console.log(nodes)
-  return (
-    // {/* <primitive object={scene} /> */}
-    <group dispose={null}>
+  function Model() {
+    return (
+      // {/* <primitive object={scene} /> */}
+      <group dispose={null}>
         <mesh
-        scale={8}
-        position={[0, -1, 0]}
-        castShadow
-        receiveShadow
-        geometry={nodes.ObjectCapture.geometry}
-        material={nodes.ObjectCapture.material}
-      />
-    </group>
-  )
-}
-  
+          scale={8}
+          position={[0, -1, 0]}
+          castShadow
+          receiveShadow
+          geometry={nodes.ObjectCapture.geometry}
+          material={nodes.ObjectCapture.material}
+        />
+      </group>
+    );
+  }
+
   return (
     <div className="nab">
       {/* Render the glTF scene */}
